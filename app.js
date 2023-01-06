dcfcc5f2e3e925af6d481ba51ce3587ca1c46bf5
baigal <osmply@gmail.com>
2023-01-06 13:21:42 +0800

    var, datatype

---
+++ app.js
@@ -0,0 +1,42 @@
+/* JS үзэх хичээлүүд
+variable, datatype, coersion, alert function, prompt function, git
+condition
+loop
+function
+array
+object
+es6
+DOM
+OOP
+Async
+*/
+// 1.06
+// variable ==>
+// var tulhuur ug ashiglaj zarlana.
+// var nas = 12;
+// nas = 14; ==>overring
+// huwisagchiin ner toogoor ehelsen baij bolohgui.
+// Olon ugnees togtoh nertei huwisagchiig CamelCase ashiglan bichne.
+
+// js datatype
+// 1. number
+// 2. string
+// 3. boolean(true, false)
+// undefined
+// null
+
+/* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence */
+
+/*Дасгал 1
+Өөрийн төрсөн оноо хувьсагчинд хадгалаад одоо байгаа оноос хасч насаа гаргаж ирдэг програм бич.
+*/
+var torsonOn = +prompt("Ta torson onoo oruulna uu?");
+var currentYear = 2023;
+var nas = currentYear - torsonOn;
+alert("Tanii nas: " + nas);
+
+/*Дасгал 2
+Гараас радиус авж тухайн радиустай тойргийн урт, талбайг тус тусад нь ол..
+urt = PiToo * 2 * r;
+talbai = PiToo * r * r;
+*/
