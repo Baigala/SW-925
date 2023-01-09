5519f26605b48af7cd51a1bf19109c1ef9a63c18
baigal <osmply@gmail.com>
2023-01-09 12:06:30 +0800

    if statement

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ DOM
 OOP
 Async
 */
+
 // 1.06
 // variable ==>
 // var tulhuur ug ashiglaj zarlana.
@@ -30,13 +31,45 @@ Async
 /*Дасгал 1
 Өөрийн төрсөн оноо хувьсагчинд хадгалаад одоо байгаа оноос хасч насаа гаргаж ирдэг програм бич.
 */
-var torsonOn = +prompt("Ta torson onoo oruulna uu?");
-var currentYear = 2023;
-var nas = currentYear - torsonOn;
-alert("Tanii nas: " + nas);
+// var torsonOn = +prompt("Ta torson onoo oruulna uu?");
+// var currentYear = 2023;
+// var nas = currentYear - torsonOn;
+// alert("Tanii nas: " + nas);
 
 /*Дасгал 2
 Гараас радиус авж тухайн радиустай тойргийн урт, талбайг тус тусад нь ол..
 urt = PiToo * 2 * r;
 talbai = PiToo * r * r;
 */
+
+// 1.09 condition, algorithm
+// if statement
+// { } - block, ugalzan haalt
+// if (nohtsol) dotorh code true buyu biyelwel {} dotorh mor code ajillana. false buyu biyelku bh yum bol else - iin ariin {} haaltan dotorh mor code ajillana.
+// if statement butets ==>
+// if (nohtsol) {
+//   // true
+// } else {
+//   // false
+// }
+// herwee 18-s doosh nasnii hun bol console-d huuhed gej hewlene. 18s deesh bol tom hun gej hewlene.
+var nas = +prompt("Nasaa oruulna uu?");
+// if (nas <= 18) {
+//   console.log("huuhed bna.");
+// } else {
+//   console.log("tom hun bna.");
+// }
+
+// 0-12 hurtelh nasnii huniig huuhed, 12-18 nastai huniig oswor nasnii huuhed, 18-c deesh bol tom hun.
+// else if - herwee ugui bol
+if (nas < 0 && nas > 150) {
+  if (nas >= 0 && nas <= 12) {
+    console.log("huuhed.");
+  } else if (nas > 12 && nas <= 18) {
+    console.log("oswor nasnii huuhed.");
+  } else {
+    console.log("tom hun.");
+  }
+} else {
+  console.log("buruu nas oruulsan bna.");
+}
